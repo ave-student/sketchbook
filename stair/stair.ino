bcfe5d640a8f2c327ab421c9b77e15d8d8af807a
Aleksandr Ermolaev <free-is@ya.ru>
2016-09-21 23:38:56 +0500

    On branch dev Changes to be committed: 	modified:   stair.ino

--- stair/stair.ino
+++ stair/stair.ino
@@ -8,49 +8,68 @@
 // Пин зумера
 #define zoomer 12
 
-// Пины сенсоров
-#define sensor_up 3
-#define sensor_down 2
-
 // Направление движения
 #define UP 1
 #define DOWN 0
 
 // Матрица светодиодов
-int steps[] = {led1, led2, led3, led4, led5};
+int steps[5] = {led1, led2, led3, led4, led5};
+
+// Структура датчиков
+struct sensor {
+  byte pin;
+  byte prev_state;
+  byte state;
+  byte last_state;
+  long last_time;
+};
 
 // Переменные сенсоров
-int prev_state = digitalRead(sensor_down);
+sensor sensor_down = {
+  2,
+  HIGH,
+  HIGH,
+  HIGH,
+  0
+};
+
+sensor sensor_up = {
+  3,
+  HIGH,
+  HIGH,
+  HIGH,
+  0
+};
 
 // Переменные для функции устранения дребезга контактов
-int state;
-int last_state = HIGH;
-long last_time = 0;
 int debounce_delay = 50;
+
+// Параметры
+int step_time = 3000;
 
 void setup() {
   for (int i = 0; i <= 4; i += 1) {
     pinMode(steps[i], OUTPUT);
     digitalWrite(steps[i], HIGH);
   }
-  pinMode(sensor_up, INPUT);
-  pinMode(sensor_down, INPUT);
-  digitalWrite(sensor_up, HIGH);
-  digitalWrite(sensor_down, HIGH);
+  pinMode(sensor_up.pin, INPUT);
+  pinMode(sensor_down.pin, INPUT);
+  digitalWrite(sensor_up.pin, HIGH);
+  digitalWrite(sensor_down.pin, HIGH);
 }
   
 void loop() {
-  int cur_state = debounce_read(sensor_down);
-  if (prev_state == HIGH) {
-    if (cur_state == LOW) {
+  debounce_read(sensor_down);
+  if (sensor_down.prev_state == HIGH) {
+    if (sensor_down.state == LOW) {
       switch_led(led1);
       tone(zoomer, 5000);
     }
   }
-  if (cur_state == HIGH) {
+  if (sensor_down.state == HIGH) {
     noTone(zoomer);
   }
-  prev_state = cur_state;
+  sensor_down.prev_state = sensor_down.state;
 }
 
 void switch_led(int led) {
@@ -58,27 +77,26 @@ void switch_led(int led) {
 }
 
 // Функция устранения дребезга контактов
-int debounce_read(int pin) {
-  int reading = digitalRead(pin);
+void debounce_read(sensor sens) {
+  int reading = digitalRead(sens.pin);
   
-  if (reading != last_state) {
-    last_time = millis();
+  if (reading != sens.last_state) {
+    sens.last_time = millis();
   }
   
-  if ((millis() - last_time) > debounce_delay) {
-    if (reading != state) {
-      state = reading;
+  if ((millis() - sens.last_time) > debounce_delay) {
+    if (reading != sens.state) {
+      sens.state = reading;
     }
   }
-  last_state = reading;
-  
-  return state;
+  sens.last_state = reading;
 }
 
 void on_led_up(int direction) {
   if (direction == UP) {
-    for (int step = 0; step <=4; step += 1) {
-      digitalWrite(steps[step], HIGH);
+    for (int step = 0; step <=4; step++) {
+      digitalWrite(steps[step], LOW);
+	  delay(step_time);
     }
   }
 }
