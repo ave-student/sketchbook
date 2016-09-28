193836b0f13f2bf4ecb9f8ffb82ac68c810c1f87
free-student <free-is@ya.ru>
2016-09-28 17:32:47 +0500

    On branch dev Changes to be committed: 	deleted:    libraries/DebounceInput/DigitalInput.cpp 	new file:   libraries/DigitalInput/DigitalInput.cpp 	renamed:    libraries/DebounceInput/DigitalInput.h -> libraries/DigitalInput/DigitalInput.h 	new file:   libraries/DigitalInput/examples/click/click.pde 	new file:   libraries/DigitalInput/keywords.txt 	new file:   libraries/DigitalInput/library.json 	new file:   libraries/DigitalInput/library.properties 	modified:   stair/stair.ino

--- libraries/DigitalInput/DigitalInput.h
+++ libraries/DigitalInput/DigitalInput.h
@@ -13,10 +13,11 @@ class DigitalInput {
 	public:
 		DigitalInput(byte pin);
 		DigitalInput(byte pin, long debounceTime);    // конструктор
-		boolean read();    // чтение сигнала на данном входе
-		boolean debounceRead();    // чтение сигнала на входе с учетом дребезга контактов
-		boolean onClicked();    // кратковременная подача сигнала
-		boolean onPressed();    // длительное удержание активного состояния
+		boolean read();    // чтение сигнала на входе с учетом дребезга контактов
+		boolean clicked();    // передний фронт сигнала
+		boolean pressed();    // длительное удержание активного состояния
+		boolean released();    // задний фронт сигнала
+		byte getPin();    // получить номер пина
 		long getDebounceTime();    // получить время демпфирования
 		void setDebounceTime(long time);    // установить время демпфирования
 
@@ -24,9 +25,12 @@ class DigitalInput {
 		byte _pin;    // номер пина
 		long _debounceTime;    // время демпфирования
 		long _lastTime;    // время последнего изменения состояния входа
-		boolean _state;    // состояние входа
-		boolean _lastState;    // последнее состояние входа
+		boolean _state;    // текущее установившееся состояние сигнала
+		boolean _prevState;    // предыдущее установившееся состояние сигнала
+		boolean _lastState;    // последнее мгновенное состояние сигнала
+		boolean _readPin();    // чтение сигнала на данном входе
+		void _setPin(byte pin);    // инициализирует пин как дискретный вход
 		void _initVar();    // инициализация переменных
-}
+};
 
 #endif
--- stair/stair.ino
+++ stair/stair.ino
@@ -1,3 +1,5 @@
+#include "DigitalInput.h"
+
 // Пины светодиодов
 #define led1 5
 #define led2 6
@@ -12,91 +14,51 @@
 #define UP 1
 #define DOWN 0
 
+// Переменные для функции устранения дребезга контактов
+int debounceDelay = 50;
+
+// Параметры
+int stepTime = 3000;
+
 // Матрица светодиодов
 int steps[5] = {led1, led2, led3, led4, led5};
 
-// Структура датчиков
-struct sensor {
-  byte pin;
-  byte prev_state;
-  byte state;
-  byte last_state;
-  long last_time;
-};
-
-// Переменные сенсоров
-sensor sensor_down = {
-  2,
-  HIGH,
-  HIGH,
-  HIGH,
-  0
-};
-
-sensor sensor_up = {
-  3,
-  HIGH,
-  HIGH,
-  HIGH,
-  0
-};
-
-// Переменные для функции устранения дребезга контактов
-int debounce_delay = 50;
-
-// Параметры
-int step_time = 3000;
+// Сенсоры
+DigitalInput sensorDown(2, debounceDelay);
+DigitalInput sensorMiddle(3, debounceDelay);
+DigitalInput sensorUp(4, debounceDelay);
 
 void setup() {
-  for (int i = 0; i <= 4; i += 1) {
-    pinMode(steps[i], OUTPUT);
-    digitalWrite(steps[i], HIGH);
-  }
-  pinMode(sensor_up.pin, INPUT);
-  pinMode(sensor_down.pin, INPUT);
-  digitalWrite(sensor_up.pin, HIGH);
-  digitalWrite(sensor_down.pin, HIGH);
+	for (int i = 0; i <= 4; i += 1) {
+		pinMode(steps[i], OUTPUT);
+		digitalWrite(steps[i], HIGH);
+	}
+
+	Serial.begin(9600);
 }
-  
+
 void loop() {
-  debounce_read(sensor_down);
-  if (sensor_down.prev_state == HIGH) {
-    if (sensor_down.state == LOW) {
-      switch_led(led1);
-      tone(zoomer, 5000);
-    }
-  }
-  if (sensor_down.state == HIGH) {
-    noTone(zoomer);
-  }
-  sensor_down.prev_state = sensor_down.state;
+	if (sensorDown.clicked()) {
+		Serial.println("Sensor clicked...");
+		switchLed(led1);
+		tone(zoomer, 5000);
+	}
+
+	if (sensorDown.released()) {
+		Serial.println("Sensor released...");
+		noTone(zoomer);
+	}
 }
 
-void switch_led(int led) {
-  digitalWrite(led, !digitalRead(led));
+void switchLed(int led) {
+	digitalWrite(led, !digitalRead(led));
 }
 
-// Функция устранения дребезга контактов
-void debounce_read(sensor sens) {
-  int reading = digitalRead(sens.pin);
-  
-  if (reading != sens.last_state) {
-    sens.last_time = millis();
-  }
-  
-  if ((millis() - sens.last_time) > debounce_delay) {
-    if (reading != sens.state) {
-      sens.state = reading;
-    }
-  }
-  sens.last_state = reading;
-}
-
-void on_led_up(int direction) {
-  if (direction == UP) {
-    for (int step = 0; step <=4; step++) {
-      digitalWrite(steps[step], LOW);
-	  delay(step_time);
-    }
-  }
+void onLedUp(int direction) {
+	if (direction == UP) {
+		for (int step = 0; step <=4; step++) {
+			digitalWrite(steps[step], LOW);
+			delay(stepTime);
+		}
+	}
 }
