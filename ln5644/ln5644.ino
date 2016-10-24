0ce81ecdfbbd5c469acf1d70312353a187cda178
free-student <free-is@ya.ru>
2016-10-24 18:23:29 +0500

    On branch dev Changes to be committed: 	new file:   .gitignore 	new file:   libraries/LN5644/examples/timer/Timer.cpp 	new file:   libraries/LN5644/examples/timer/Timer.h 	renamed:    ln5644/ln5644.ino -> libraries/LN5644/examples/timer/timer.ino 	modified:   ln5644/ln5644.ino

--- ln5644/ln5644.ino
+++ ln5644/ln5644.ino
@@ -1,21 +1,28 @@
 #include "LN5644.h"
 #include "Timer.h"
 
-#define bStart 10
-#define bPause 11
-#define bStop 12
-#define zoomer A4
+#define zoomer 12
+#define buttons A4
+
+#define but1 514
+#define but2 599
+#define but3 655
+#define but12 747
+#define but13 791
+#define but23 801
+#define but123 866
+
+#define lim 4
 
 int an[4] = {A0, A1, A2, A3};
-int cat[8] = {2, 3, 4, 5, 6, 7, 8, 9}; 
+int cat[8] = {7, 9, 5, 3, 2, 8, 6, 4}; 
+
+int value;
 
 LN5644 disp;
-Timer timer;
+/* Timer timer; */
 
 void setup() {
-	pinMode(bStart, INPUT_PULLUP);
-	pinMode(bStop, INPUT_PULLUP);
-	pinMode(bPause, INPUT_PULLUP);
 	pinMode(zoomer, OUTPUT);
 	digitalWrite(zoomer, LOW);
 	
@@ -27,21 +34,67 @@ void setup() {
 
 void loop() {
 	disp.next();
-	if (!digitalRead(bStart)) {
-		timer.start();
-	}
-	if (!digitalRead(bPause)) {
-		timer.pause();
-	}
-	if (!digitalRead(bStop)) {
-		timer.stop();
-	}
-	disp.display(timer.next());
-	if ((timer.next() > 0) && (timer.next() % 60) == 0) {
-		tone(zoomer, 5000);
-	}
-	else {
-		noTone(zoomer);
-	}	
+	value = analogRead(buttons);
+	disp.display(value);
+
+	/* if (value < 100) { */
+	/* 	disp.clear(); */
+	/* 	disp.display(0, Nb); */
+	/* } */
+	/* else { */
+	/* 	displayButtons(value); */
+	/* } */
+
+	/* if (!digitalRead(bStart())) { */
+	/* 	timer.start(); */
+	/* } */
+	/* if (!digitalRead(bPause())) { */
+	/* 	timer.pause(); */
+	/* } */
+	/* if (!digitalRead(bStop())) { */
+	/* 	timer.stop(); */
+	/* } */
+	/* disp.display(timer.next()); */
+	/* if ((timer.next() > 0) && (timer.next() % 60) == 0) { */
+	/* 	tone(zoomer, 5000); */
+	/* } */
+	/* else { */
+	/* 	noTone(zoomer); */
+	/* }	 */
+	
 }
 
+bool inRange(int num, int range, int limit) {
+	if ((num > (range - limit)) && (num < (range + limit)))
+		return true;
+	return false;
+}
+
+void displayButtons(int value) {
+	if (inRange(value, but1, lim)) {
+		disp.display(3, N8);
+	}
+	if (inRange(value, but2, lim)) {
+		disp.display(2, N8);
+	}
+	if (inRange(value, but3, lim)) {
+		disp.display(1, N8);
+	}
+	if (inRange(value, but12, lim)) {
+		disp.display(3, N8);
+		disp.display(2, N8);
+	}
+	if (inRange(value, but13, lim)) {
+		disp.display(3, N8);
+		disp.display(1, N8);
+	}
+	if (inRange(value, but23, lim)) {
+		disp.display(3, N8);
+		disp.display(2, N8);
+	}
+	if (inRange(value, but123, lim)) {
+		disp.display(3, N8);
+		disp.display(2, N8);
+		disp.display(1, N8);
+	}
+}
