fe6ab23b7345be8580570ecc936ecc8e05df77bb
free-student <free-is@ya.ru>
2016-10-24 22:46:04 +0500

    On branch dev1 Changes to be committed: 	modified:   libraries/LN5644/LN5644.cpp 	modified:   ln5644/ln5644.ino

--- ln5644/ln5644.ino
+++ ln5644/ln5644.ino
@@ -1,18 +1,12 @@
 #include "LN5644.h"
 #include "Timer.h"
+#include "IOLib.h"
 
-#define zoomer 12
 #define buttons A4
 
-#define but1 514
-#define but2 599
-#define but3 655
-#define but12 747
-#define but13 791
-#define but23 801
-#define but123 866
-
 #define lim 4
+
+SDigitalOutput zoomer(12, LOW);
 
 SDigitalOutput a0(A0);
 SDigitalOutput a1(A1);
@@ -36,9 +30,6 @@ LN5644 disp;
 /* Timer timer; */
 
 void setup() {
-	pinMode(zoomer, OUTPUT);
-	digitalWrite(zoomer, LOW);
-	
 	disp.init();
 
 	disp.setAnods(an);
@@ -49,14 +40,6 @@ void loop() {
 	disp.next();
 	value = analogRead(buttons);
 	disp.display(value);
-
-	/* if (value < 100) { */
-	/* 	disp.clear(); */
-	/* 	disp.display(0, Nb); */
-	/* } */
-	/* else { */
-	/* 	displayButtons(value); */
-	/* } */
 
 	/* if (!digitalRead(bStart())) { */
 	/* 	timer.start(); */
@@ -75,39 +58,4 @@ void loop() {
 	/* 	noTone(zoomer); */
 	/* }	 */
 	
-}
-
-bool inRange(int num, int range, int limit) {
-	if ((num > (range - limit)) && (num < (range + limit)))
-		return true;
-	return false;
-}
-
-void displayButtons(int value) {
-	if (inRange(value, but1, lim)) {
-		disp.display(3, N8);
-	}
-	if (inRange(value, but2, lim)) {
-		disp.display(2, N8);
-	}
-	if (inRange(value, but3, lim)) {
-		disp.display(1, N8);
-	}
-	if (inRange(value, but12, lim)) {
-		disp.display(3, N8);
-		disp.display(2, N8);
-	}
-	if (inRange(value, but13, lim)) {
-		disp.display(3, N8);
-		disp.display(1, N8);
-	}
-	if (inRange(value, but23, lim)) {
-		disp.display(3, N8);
-		disp.display(2, N8);
-	}
-	if (inRange(value, but123, lim)) {
-		disp.display(3, N8);
-		disp.display(2, N8);
-		disp.display(1, N8);
-	}
 }
