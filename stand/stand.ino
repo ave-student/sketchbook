2227e77bfe82cc8ce89e797a6f0c4477ef5f3a00
Aleksandr Ermolaev <free-is@ya.ru>
2016-11-20 19:29:20 +0500

    Input module is done!

--- stand/stand.ino
+++ stand/stand.ino
@@ -2,33 +2,20 @@
 // ### Демонстрация роботы регистра 74CH165 ###
 //********************************************************************//
 
-#include "LN5644.h"
+// Пин 9 Q
+#define pinData 11
 
-#define dP 2
-#define cP 3
-#define lP 4
+// Пин 7 Q/
+#define pinDataInv 12
 
-#define Pin_REGDATA 11
-#define Pin_REGDATAINVERT 12
-#define Pin_REGCLOCK 8 
-#define Pin_REGCHECK 7
-// Номер пина на плате к которому будет подключено: Вывод включения микросхемы
-// Пин 15  СE/
-#define Pin_REGACTIV 5
+// Пин 2_CP
+#define pinClock 8 
+
+// Пин 1 PL/
+#define pinLatch 7
+
 // Количество регистров
-#define REGISTR_COUNT 1
-
-// Состояние шини синхронизации
-#define REGCLOCK_HIGH HIGH // высокий уровень на шине
-#define REGCLOCK_LOW  LOW  // низкий уровень на шине
-
-// Состояние защелки
-#define REGCHECK_ON  HIGH // Защелка включена
-#define REGCHECK_OFF LOW  // Защелка выключена
-
-// Состояние роботи регистра
-#define REGACTIV_ON  LOW   // триггер сброшен
-#define REGACTIV_OFF HIGH  // тригер не сброшен
+#define REGISTR_COUNT 2
 
 // Принятые данные
 int INDATA[7*REGISTR_COUNT+1];
@@ -36,76 +23,72 @@ int INDATA[7*REGISTR_COUNT+1];
 // Принятые данные инверсные
 int INDATAINVERT[7*REGISTR_COUNT+1];
 
-LN5644 disp;
-
-void setup()
-{
+void setup() {
 	// Настройка вывода платы в режим "Выход"
-	pinMode(Pin_REGCLOCK, OUTPUT);  
-	pinMode(Pin_REGCHECK, OUTPUT);
-	pinMode(Pin_REGACTIV, OUTPUT);
+	pinMode(pinClock, OUTPUT);  
+	pinMode(pinLatch, OUTPUT);
 
 	// Настройка вывода платы в режим "Вход"  
-	pinMode(Pin_REGDATA, INPUT);
-	pinMode(Pin_REGDATAINVERT, INPUT);
-
-	// Устанавливаем состояние регистра: NoActiv 
-	digitalWrite(Pin_REGACTIV, REGACTIV_OFF); 
+	pinMode(pinData, INPUT);
+	pinMode(pinDataInv, INPUT);
 
 	// Устанавливаем состояние защелки: OFF
-	digitalWrite(Pin_REGCHECK, REGCHECK_OFF);    
+	digitalWrite(pinLatch, LOW);    
+
 	// Инициализация SerialPort
 	Serial.begin(9600);    
-	disp.init();
-	disp.setPins(dP, cP, lP);
+}
+	 
+void loop() {
+	Serial.println("fedcba9876543210");
+	Serial.println(shiftInReg(pinData, pinClock, pinLatch), BIN);
+	Serial.println();
+	delay(1000); 
 }
 
-void loop()
-	{
-	disp.next();
+int shiftInReg(int dataPin, int clockPin, int latchPin) {
+	int data = 0;
+	int temp;
+
 	// Устанавливаем состояние защелки: ON
-	digitalWrite(Pin_REGCHECK, REGCHECK_ON);
-	// Устанавливаем состояние регистра: Activ 
-	digitalWrite(Pin_REGACTIV, REGACTIV_ON); 
+	digitalWrite(latchPin, HIGH);
 
 	// Прием данных с регистра
-	for(int i = 0; i <7*REGISTR_COUNT+1; i++)
-	{
-		// Получаем данные
-		INDATA[i] = digitalRead(Pin_REGDATA);
-		// Получаем данные инверсные
-		INDATAINVERT[i] = digitalRead(Pin_REGDATAINVERT);
+	for(int i = 0; i < 7 * REGISTR_COUNT + 1; i++) {
+		temp = digitalRead(dataPin);
+		if (temp) {
+			data = (data << 1) | 1;
+		}
+		else {
+			data = data << 1;
+		}
 
-		// Посилаем такт синхронизации
-		digitalWrite(Pin_REGCLOCK, REGCLOCK_HIGH);
-		digitalWrite(Pin_REGCLOCK, REGCLOCK_LOW);  
+		/* INDATA[i] = digitalRead(pinData); */
+		
+		// Получаем данные инверсные
+		/* INDATAINVERT[i] = digitalRead(pinDataInv); */
+
+		// Посылаем такт синхронизации
+		digitalWrite(clockPin, HIGH);
+		digitalWrite(clockPin, LOW);  
 	}
 
-	// Устанавливаем состояние регистра: NoActiv 
-	digitalWrite(Pin_REGACTIV, REGACTIV_OFF); 
-
 	// Устанавливаем состояние защелки: OFF
-	digitalWrite(Pin_REGCHECK, REGCHECK_OFF);
+	digitalWrite(latchPin, LOW);
+
+	return data;
 
 	// Выводим данные в терминал
-	for(int i = 0; i <7*REGISTR_COUNT+1; i++)
-	{
-		// Получаем данные
-		Serial.print(INDATA[i]);
-	}    
+	/* for(int i = 0; i <7*REGISTR_COUNT+1; i++) { */
+	/* 	// Получаем данные */
+	/* 	Serial.print(INDATA[i]); */
+	/* }     */
+	/* Serial.print(": INDATA"); */
 
-	Serial.println(": INDATA");
-	
 	// Выводим данные инверсные в терминал
-	for(int i = 0; i <7*REGISTR_COUNT+1; i++)
-	{
-		// Получаем данные
-		Serial.print(INDATAINVERT[i]);
-	}    
-
-	Serial.println(": INDATAINVERT");
-
-	// Задержка времени
-	delay(1000); 
-	disp.display(1111);
+	/* for(int i = 0; i <7*REGISTR_COUNT+1; i++) { */
+	/* 	// Получаем данные */
+	/* 	Serial.print(INDATAINVERT[i]); */
+	/* }     */
+	/* Serial.print(": INDATAINVERT"); */
 }
