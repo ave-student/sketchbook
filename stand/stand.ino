486b24087917a077319ca09ba69e17e38533ec0a
free-student <free-is@ya.ru>
2016-12-06 00:35:41 +0500

    Stand v0.1

--- stand/stand.ino
+++ stand/stand.ino
@@ -2,41 +2,30 @@
 // ### Демонстрация роботы регистра 74CH165 ###
 //********************************************************************//
 
-// Пин 9 Q
-#define pinData 11
-
-// Пин 7 Q/
-#define pinDataInv 12
-
-// Пин 2_CP
-#define pinClock 10 
-
-// Пин 1 PL
-#define pinLatch 9
-
-// Количество регистров
-#define REGISTR_COUNT 2
-
 #include "Wire.h"
 #include "LiquidCrystal_I2C.h"
-#include "DI165.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
+
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature teSensor(&oneWire);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-DI165 dim(pinDataInv, pinClock, pinLatch, REGISTR_COUNT);
-
-long bits;
 
 void setup() {
 	lcd.init();
 	lcd.backlight();
-	lcd.print("74HC165 input");
+	lcd.print("Stand v0.1");
+	teSensor.begin();
+	lcd.setCursor(0, 1);
+	lcd.print("Tout = ");
 }
 	 
 void loop() {
-	bits = dim.shiftIn(0);
-	bits = bits << 8;
-	bits = bits | dim.shiftIn(1);
-
-	lcd.setCursor(0, 1);
-	lcd.print(bits, BIN);
+	teSensor.requestTemperatures();
+	lcd.setCursor(7, 1);
+	lcd.print(teSensor.getTempCByIndex(0));  
 }
